f42c14ab92db782b7bf17cb59fae10b620b1271d
Olivier Prouvost <olivier.prouvost@opcoach.com>
2014-09-21 23:47:51 +0200

    Change command and menu.

---
+++ com.opcoach.gendoc/src/com/opcoach/gendoc/DocumentGeneratorMenuContribution.java
@@ -0,0 +1,29 @@
+package com.opcoach.gendoc;
+
+import org.eclipse.jface.action.ContributionItem;
+
+
+/** This class must fill the menu set on an EObject. 
+ *   It gets all generator and check if the targetClass is compliant with selected class. 
+ *   If so, it add the generator in the menu. 
+ * @author olivier
+ *
+ */
+public class DocumentGeneratorMenuContribution extends ContributionItem
+{
+
+	public DocumentGeneratorMenuContribution()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public DocumentGeneratorMenuContribution(String id)
+	{
+		super(id);
+		// TODO Auto-generated constructor stub
+		
+		
+		
+	}
+
+}
